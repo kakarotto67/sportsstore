6c3487c4440d99461d9afd93607f60bc0d131c2f
Roman Vovk <arromanvk@gmail.com>
2019-05-26 22:40:31 +0300

    Initial commit

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DataContext context;
+
+        public HomeController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Sports Store App";
+
+            var model = context.Products.FirstOrDefault();
+
+            return View(model);
+        }
+    }
+}
